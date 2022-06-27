58ef46d8ad837f941317c91233839ec023f80f4c
Aahan Gandhi <72563048+aahangandhi@users.noreply.github.com>
2022-06-27 16:15:25 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAXsjB3_9gYcNo2ICUjuAaZ7J42zIhSPVY",
+  authDomain: "storytelling-app-b3514.firebaseapp.com",
+  databaseURL: "https://storytelling-app-b3514-default-rtdb.firebaseio.com",
+  projectId: "storytelling-app-b3514",
+  storageBucket: "storytelling-app-b3514.appspot.com",
+  messagingSenderId: "92828033763",
+  appId: "1:92828033763:web:bd6f0ed0958dca42bcdbd6"
+  };
